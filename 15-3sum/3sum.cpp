50137a5480022244fcac4a86a9695076d84f3976
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2024-12-14 15:39:16 +0530

    Time: 48 ms (68.02%) | Memory: 29.1 MB (50.32%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0) {
+                    // we need to decrease the sum therefore reduce k
+                    k--;
+                } else if (sum < 0) {
+                    // increase sum
+                    j++;
+                } else {
+                    // found a pair
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    j++, k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                }
+            }
+        }
+        return res;
+    }
+};
