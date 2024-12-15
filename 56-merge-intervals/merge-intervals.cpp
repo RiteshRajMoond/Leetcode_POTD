6c2a0af541fbf18e9380e108b8be8d30aa02d01a
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2024-12-15 11:13:51 +0530

    Time: 5 ms (56.19%) | Memory: 23.9 MB (34.33%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& inter) {
         vector<vector<int>> res;
-
-        // sort
         sort(inter.begin(), inter.end());
-
         for(auto& i : inter) {
             if(res.empty() || res.back()[1] < i[0]) res.push_back(i);
             else res.back()[1] = max(res.back()[1], i[1]);
