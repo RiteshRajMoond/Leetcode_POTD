22f98dbbe496380444b90de8828577a5815d1ee3
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2024-12-19 09:01:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (7.32%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        int maxChunk = 0;
-        int maxValue = 0;
-
-        for (int i = 0; i < arr.size(); i++) {
-            maxValue = max(maxValue, arr[i]);
-
-            if (i == maxValue) {
-                maxChunk++;
-            }
+        stack<int> st; // ele
+        for(int& i : arr) {
+            if(!st.empty() && i < st.top()) {
+                int tp = st.top();
+                while(!st.empty() && i < st.top()) st.pop();
+                st.push(tp);
+            } else st.push(i);
         }
 
-        return maxChunk;
+        return st.size();
     }
 };
